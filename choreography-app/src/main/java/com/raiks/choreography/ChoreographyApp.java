b70384fc09054f607d790bb19eb2d654b64a1f8c
Sergei Retsia <Sergei_Retsia@epam.com>
2020-05-22 22:55:16 +0300

    Added the choreograph-app module content (POM + ChoreographyApp)

---
+++ choreography-app/src/main/java/com/raiks/choreography/ChoreographyApp.java
@@ -0,0 +1,36 @@
+package com.raiks.choreography;
+
+import java.util.Collections;
+import java.util.List;
+
+import com.raiks.choreography.UndirectedGraph;
+
+public class ChoreographyApp {
+    public static void main(String[] args) {
+        UndirectedGraph<String> graph = new UndirectedGraph<>();
+        graph.addVertex("Boston");
+        graph.addVertex("New York");
+        graph.addVertex("Charlotte");
+        graph.addVertex("Nashville");
+        graph.addVertex("Atlanta");
+        graph.addVertex("Honolulu");
+
+        graph.addEdge("Boston", "New York");
+        graph.addEdge("New York", "Nashville");
+        graph.addEdge("New York", "Charlotte");
+        graph.addEdge("Charlotte", "Atlanta");
+        graph.addEdge("Nashville", "Atlanta");
+
+        System.out.println("You can travel from Boston to Atlanta via:");
+        List<String> path = graph.findPath("Boston", "Atlanta");
+        Collections.reverse(path);
+        System.out.println(path);
+
+        System.out.println("----------------------");
+
+        System.out.println("You can travel from Boston to Honolulu via:");
+        path = graph.findPath("Boston", "Honolulu");
+        Collections.reverse(path);
+        System.out.println(path);
+    }
+}
